01159922c011c59e864236381509e678a9be5d30
Prince Jain <138282285+PRINCEjain0@users.noreply.github.com>
2025-07-22 22:13:11 +0530

    Update redis.js

--- redis.js
+++ redis.js
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 export const redis = new Redis(
-  "redis-cli --tls -u redis://default:AXkdAAIjcDFlMzRmODg2M2U3YmQ0MTE1YjFhNjM3YmYxYmE0YjBmM3AxMA@touching-sparrow-31005.upstash.io:6379",
+  "redis://default:AXkdAAIjcDFlMzRmODg2M2U3YmQ0MTE1YjFhNjM3YmYxYmE0YjBmM3AxMA@touching-sparrow-31005.upstash.io:6379",
   {
     maxRetriesPerRequest: null,
   }
